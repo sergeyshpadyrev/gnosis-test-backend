4fcf31cb947dba91073c1819a7cb31315850cb76
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-02-27 23:40:39 +0400

    Add verification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/authenticate')
+  async authenticate(@Body() body: { message: string; signature: string }) {
+    const token = await this.appService.authenticate(body);
+    if (!token) throw new UnauthorizedException();
+    return { token };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { SiweMessage } from 'siwe';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async authenticate(props: { message: string; signature: string }) {
+    const siweMessage = new SiweMessage(props.message);
+    const verified = await siweMessage.verify({ signature: props.signature });
+
+    if (!verified.success) return null;
+
+    return 'token';
   }
 }
