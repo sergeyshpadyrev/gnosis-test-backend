5cc02709f45422b76526c38b4b738839b44de9bd
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-03-01 20:23:58 +0400

    Fix vercel

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import * as bodyParser from 'body-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.enableCors({ origin: '*' });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
+
 bootstrap();
+module.exports = server;
